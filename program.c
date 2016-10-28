80ac8a2514d926d7380c913b7ab3a80399c4a805
Thiago Scalone <thiago@scalone.com.br>
2016-10-27 22:00:48 -0200

    First commit

---
+++ program.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+  if (argc >= 2) {
+    if (strcmp(argv[1], "thiago.scalone") == 0) {
+      printf("\nCowabunga!\n");
+    }
+  }
+}
