79ce3d97f0ce26cc388644e1a974c758578ab954
Aluno <aluno@fatec.local>
2016-10-27 22:39:31 -0200

    Adicionado a entrada AndreiaAlves

--- program.c
+++ program.c
@@ -8,5 +8,9 @@ int main(int argc, char *argv[])
     if (strcmp(argv[1], "thiago.scalone") == 0) {
       printf("\nCowabunga!\n");
     }
+    if (strcmp(argv[1], "AndreiaAlves") == 0) {
+      printf("\nHello world Git!\n");
+    }
+
   }
 }
